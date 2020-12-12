788467e25f6825597186d82b5bbb593c70668791
odlovesq <reason.hot00@gmail.com>
2020-12-13 03:57:35 +0900

    add: 家計簿作成API作成

--- app/controllers/api/v1/household_account_books_controller.rb
+++ app/controllers/api/v1/household_account_books_controller.rb
@@ -1,7 +1,15 @@
 module Api::V1
   class HouseholdAccountBooksController < ApiController
     def create
-      render json: { status: 'SUCCESS', data: 'hoge' }
+      householdAccountBook = HouseholdAccountBook.new(household_account_book_params.merge(user_ids: current_user.id))
+      householdAccountBook.save!
+      render status: 201, json: householdAccountBook
+    end
+
+    private
+
+    def household_account_book_params
+      params.permit(:name)
     end
   end
 end
--- app/controllers/api_controller.rb
+++ app/controllers/api_controller.rb
@@ -2,11 +2,23 @@ class ApiController < ActionController::API
   include DeviseTokenAuth::Concerns::SetUserByToken
   before_action :authenticate_user!
   before_action :check_xhr_header
+  rescue_from StandardError, :with => :rescue_exception
 
   private
 
   def check_xhr_header
     return if request.xhr?
-    render json: { error: 'forbidden' }, status: :forbidden
+    render status: :forbidden, json: { error: 'forbidden' }
+  end
+
+  def rescue_exception(e)
+    @message = e.message
+    @status = validate_error?(e) ? 400 : 500
+
+    render status: @status, json: { message: @message }
+  end
+
+  def validate_error?(e)
+    return e.is_a?(ActiveRecord::RecordInvalid)
   end
 end
