8961ca4901688916701346fdefe48e36894c1940
odlovesq <reason.hot00@gmail.com>
2020-12-13 19:25:05 +0900

    update: 変数名修正

--- app/controllers/api/v1/household_account_books_controller.rb
+++ app/controllers/api/v1/household_account_books_controller.rb
@@ -1,9 +1,9 @@
 module Api::V1
   class HouseholdAccountBooksController < ApiController
     def create
-      householdAccountBook = HouseholdAccountBook.new(household_account_book_params.merge(user_ids: current_user.id))
-      householdAccountBook.save!
-      render status: 201, json: householdAccountBook
+      household_account_book = HouseholdAccountBook.new(household_account_book_params.merge(user_ids: current_user.id))
+      household_account_book.save!
+      render status: 201, json: household_account_book
     end
 
     private
