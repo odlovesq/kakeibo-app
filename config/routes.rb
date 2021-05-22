601fe5fc45951da65dfb4b1b67f8fbd8f023a26d
odlovesq <reason.hot00@gmail.com>
2021-05-22 20:03:14 +0900

    update: インデント修正

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
   scope :api, defaults: { format: 'json' } do
     scope :v1 do
       mount_devise_token_auth_for 'User', at: '/auth', controllers: {
-          registrations: 'api/v1/auth/registrations'
+        registrations: 'api/v1/auth/registrations'
       }
       post 'household_account_book', to: 'api/v1/household_account_books#create'
     end
